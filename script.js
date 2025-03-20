78d1f75c4a7b7860e8f1afb1b9542c0671e77931
Aniket Srivastava <rayquazamega75@gmail.com>
2025-03-20 20:51:12 +0530

    added working UI

--- script.js
+++ script.js
@@ -24,94 +24,123 @@ function getComputerChoice()
         return SCISSOR;
     }
 }
-function getHumanChoice()
-{
-    let choice=prompt("Rock, Paper, Scissors!");
-    const choiceign=choice.toLowerCase();
-    if(choiceign==="rock")
-    {
-        return ROCK;
-    }
-    else if(choiceign==="paper")
-    {
-        return PAPER;
-    }
-    else if(choiceign==="scissor")
-    {
-        return SCISSOR;
-    }
-    else
-    {
-        return "Invalid Choice!!";
-    }
-}
 function playRound(humanChoice, computerChoice)
 {
     if(humanChoice===computerChoice)
     {
-        console.log("Draw!!!");
         humanScore++;
         computerScore++;
+        return "Draw!!!";
     }
     else if(humanChoice==="rock")
     {
         if(computerChoice==="paper")
         {
-            console.log("You lose!! Paper beats Rock");
             computerScore++;
+            return "You lose!! Paper beats Rock";
         }
         else if(computerChoice==="scissor")
         {
-            console.log("Congratulations you win! Rock beats Scissors");
             humanScore++;
+            return "Congratulations you win! Rock beats Scissors";
         }
     }
     else if(humanChoice==="paper")
     {
         if(computerChoice==="rock")
         {
-            console.log("Congratulations you win! Paper beats Rock");
             humanScore++;
+            return "Congratulations you win! Paper beats Rock";
         }
         else if(computerChoice==="scissor")
         {
-            console.log("You Lose! Scissors beat Paper");
             computerScore++;
+            return "You Lose! Scissors beat Paper";
         }
     }
     else if(humanChoice==="scissor")
     {
         if(computerChoice==="paper")
         {
-            console.log("Congratulations you win! Scissors beat Paper");
             humanScore++;
+            return "Congratulations you win! Scissors beat Paper";
         }
         else if(computerChoice==="rock")
         {
-            console.log("You lose! Rock beats Scissors");
             computerScore++;
+            return "You lose! Rock beats Scissors";
         }
     }
 }
 
-function playGame()
+function updateUI(message)
 {
-    for(let i=0;i<5;i++)
+    document.querySelector("#result").textContent = message;
+    document.querySelector("#score").textContent = `Player Score: ${humanScore} | Computer Score: ${computerScore}`;
+}
+
+function checkWinner()
+{
+    if(humanScore >= 5 && computerScore >= 5)
     {
-        let humanSelection=getHumanChoice();
-        let computerSelection=getComputerChoice();
-        playRound(humanSelection, computerSelection);
+        document.querySelector("#result").textContent = "It's a DRAW!!!";
+        document.querySelectorAll("button").forEach(button => button.disabled = true);
+        const draw = document.createElement("img");
+        draw.src = "images/pngtree-shake-hands-png-image_8742463";
+        draw.alt = "Its a draw image!!";
+        draw.width = "200";
+        draw.heigth = "200";
+        draw.style.textAlign = "center";
+        document.body.appendChild(draw);
     }
-    console.log(`Human Score is ${humanScore}`);
-    console.log(`Computer Score is ${computerScore}`);
-    if(humanScore>computerScore)
+    if(humanScore >= 5)
     {
-        console.log("Congratulations! You won");
+        document.querySelector("#result").textContent = "Congratulations!! You win";
+        document.querySelectorAll("button").forEach(button => button.disabled = true);
+        const human = document.createElement("img");
+        human.src = "images/happy-smiling-young-man-avatar-3d-portrait-of-a-man-cartoon-character-people-illustration-isolated-on-white-background-vector";
+        human.alt = "Human";
+        human.width = "200";
+        human.heigth = "200";
+        draw.style.textAlign = "center";
+        document.body.appendChild(human);
     }
-    else if(computerScore>humanScore)
+    else if(computerScore >= 5)
     {
-        console.log("Sorry you lose!");
+        document.querySelector("#result").textContent = "Sorry, you lose!!";
+        document.querySelectorAll("button").forEach(button => button.disabled = true);
+        const computer = document.createElement("img");
+        computer.src = "images/cartoon-smiling-desktop-computer-19057432";
+        computer.alt = "Computer";
+        computer.width = "200";
+        computer.heigth = "200";
+        draw.style.textAlign = "center";
+        document.body.appendChild(computer);
     }
 }
 
-playGame();
+const h1 = document.querySelector("h1");
+h1.style.textAlign = "center";
+const container1 = document.querySelector(".container1");
+container1.style.textAlign = "center";
+const container2 = document.querySelector(".container2");
+container2.style.textAlign = "center";
+
+
+document.querySelector("#rock").addEventListener('click', () => {
+    const result = playRound(ROCK, getComputerChoice());
+    updateUI(result);
+    checkWinner();
+});
+
+document.querySelector("#paper").addEventListener('click', () => {
+    const result = playRound(PAPER, getComputerChoice());
+    updateUI(result);
+    checkWinner();
+});
+
+document.querySelector("#scissor").addEventListener('click', () => {
+    const result = playRound(SCISSOR, getComputerChoice());
+    updateUI(result);
+    checkWinner();
+});
